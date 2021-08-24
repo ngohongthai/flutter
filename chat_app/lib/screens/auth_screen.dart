e6e57619d900e155dbbd738bbb1d4500333aa0c7
Ngô Hồng Thái <ngohongthai.uet@gmail.com>
2021-08-24 23:44:53 +0700

    Add authen screen, firebase sdk

---
+++ chat_app/lib/screens/auth_screen.dart
@@ -0,0 +1,27 @@
+import 'package:chat_app/widgets/auth/auth_form.dart';
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  void _submitAuthForm(
+    String email,
+    String password,
+    String username,
+    bool isLogin,
+  ) {}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: AuthForm(
+        submitFn: _submitAuthForm,
+      ),
+    );
+  }
+}
